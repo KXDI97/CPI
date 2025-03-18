41da5cbf525930dbec2fa5c5501b565ae5233632
BRYAMS G. SOLORZANO <solorzanobryams@outlook.com>
2025-03-17 20:24:26 -0500

    version 1.1

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-   var swiper = new Swiper(".mySwiper-2", {
+var swiper = new Swiper(".mySwiper-2", {
     slidesPerView: 3, // Ajusta el número de productos visibles al mismo tiempo
     spaceBetween: 20, // Espaciado entre productos
     loop: true, // Hace que el carrusel sea infinito
