d350af9fdfc87cbbc57c6f624bd942762d3026bc
Mario <Mario@PLANETA_99>
2025-03-19 20:59:17 -0500

    version 1.1

--- Landing Page/script.js
+++ Landing Page/script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
    var swiper = new Swiper(".mySwiper-2", {
-    slidesPerView: 1, // Ajusta el número de productos visibles al mismo tiempo
+    slidesPerView: 3, // Ajusta el número de productos visibles al mismo tiempo
     spaceBetween: 20, // Espaciado entre productos
-    loop: false, // Hace que el carrusel sea infinito
+    loop: true, // Hace que el carrusel sea infinito
     loopFillGroupWithBlank: true, // 🔥 Esto evita el hueco
     navigation: {
         nextEl: ".swiper-button-next",
